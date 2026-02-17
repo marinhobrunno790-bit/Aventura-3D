9d8a9056fd1e6ce84a15576ad4ef04e469d732e6
marinhobrunno790-bit <marinhobrunno790@gmail.com>
2026-02-17 19:07:36 -0300

    Add files via upload

---
+++ Java.java
@@ -0,0 +1,14 @@
+package com.aventura3d.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    // Capacitor lida com tudo via BridgeActivity.
+    // Adicione plugins customizados aqui se necessário no futuro:
+    //
+    // @Override
+    // public void onCreate(Bundle savedInstanceState) {
+    //     registerPlugin(MeuPlugin.class);
+    //     super.onCreate(savedInstanceState);
+    // }
+}
